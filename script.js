a9d69de537b1cee23e014272e48fdc561af14f51
Suraj31-creator <zhasuraj0@gmail.com>
2025-02-18 14:40:41 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add any interactive functionality here
+// Example: You can add an alert on page load to make it more dynamic
+window.onload = function () {
+  console.log("Welcome to Suraj Kumar Jha's portfolio!");
+}
